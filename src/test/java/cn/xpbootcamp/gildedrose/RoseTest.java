a9e48c303186af0778991bdb2b3929b125393726
Phony Lou <louqilin555@gmail.com>
2020-05-17 11:39:09 +0800

    Correct variable for each case

--- src/test/java/cn/xpbootcamp/gildedrose/RoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/RoseTest.java
@@ -65,90 +65,90 @@ public class RoseTest {
     void should_sellIn_10_quality_50_when_one_day_passed_given_Sulfuras_sellIn_10_quality_50() {
         Sulfuras sulfuras = new Sulfuras(10,50);
         Rose rose = new Rose();
-        Sulfuras updatedAgedBrie = (Sulfuras) rose.oneDayPassed(sulfuras);
+        Sulfuras updatedSulfuras = (Sulfuras) rose.oneDayPassed(sulfuras);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(50);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(10);
+        assertThat(updatedSulfuras.getQuality()).isEqualTo(50);
+        assertThat(updatedSulfuras.getSellIn()).isEqualTo(10);
     }
 
     @Test
     void should_sellIn_0_quality_1_when_one_day_passed_given_Sulfuras_sellIn_0_quality_1() {
         Sulfuras sulfuras = new Sulfuras(0,1);
         Rose rose = new Rose();
-        Sulfuras updatedAgedBrie = (Sulfuras) rose.oneDayPassed(sulfuras);
+        Sulfuras updatedSulfuras = (Sulfuras) rose.oneDayPassed(sulfuras);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(1);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(0);
+        assertThat(updatedSulfuras.getQuality()).isEqualTo(1);
+        assertThat(updatedSulfuras.getSellIn()).isEqualTo(0);
     }
 
     @Test
     void should_sellIn_minus_1_quality_1_when_one_day_passed_given_Sulfuras_sellIn_minus_1_quality_1() {
         Sulfuras sulfuras = new Sulfuras(-1,1);
         Rose rose = new Rose();
-        Sulfuras updatedAgedBrie = (Sulfuras) rose.oneDayPassed(sulfuras);
+        Sulfuras updatedSulfuras = (Sulfuras) rose.oneDayPassed(sulfuras);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(1);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(-1);
+        assertThat(updatedSulfuras.getQuality()).isEqualTo(1);
+        assertThat(updatedSulfuras.getSellIn()).isEqualTo(-1);
     }
 
     @Test
     void should_sellIn_11_quality_2_when_one_day_passed_given_BackstagePass_sellIn_12_quality_1() {
         BackstagePass backstagePass = new BackstagePass(12,1);
         Rose rose = new Rose();
-        BackstagePass updatedAgedBrie = (BackstagePass) rose.oneDayPassed(backstagePass);
+        BackstagePass updatedBackstagePass = (BackstagePass) rose.oneDayPassed(backstagePass);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(2);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(11);
+        assertThat(updatedBackstagePass.getQuality()).isEqualTo(2);
+        assertThat(updatedBackstagePass.getSellIn()).isEqualTo(11);
     }
 
     @Test
     void should_sellIn_9_quality_3_when_one_day_passed_given_BackstagePass_sellIn_10_quality_1() {
         BackstagePass backstagePass = new BackstagePass(10,1);
         Rose rose = new Rose();
-        BackstagePass updatedAgedBrie = (BackstagePass) rose.oneDayPassed(backstagePass);
+        BackstagePass updatedBackstagePass = (BackstagePass) rose.oneDayPassed(backstagePass);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(3);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(9);
+        assertThat(updatedBackstagePass.getQuality()).isEqualTo(3);
+        assertThat(updatedBackstagePass.getSellIn()).isEqualTo(9);
     }
 
     @Test
     void should_sellIn_4_quality_4_when_one_day_passed_given_BackstagePass_sellIn_5_quality_1() {
         BackstagePass backstagePass = new BackstagePass(5,1);
         Rose rose = new Rose();
-        BackstagePass updatedAgedBrie = (BackstagePass) rose.oneDayPassed(backstagePass);
+        BackstagePass updatedBackstagePass = (BackstagePass) rose.oneDayPassed(backstagePass);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(4);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(4);
+        assertThat(updatedBackstagePass.getQuality()).isEqualTo(4);
+        assertThat(updatedBackstagePass.getSellIn()).isEqualTo(4);
     }
 
     @Test
     void should_sellIn_0_quality_4_when_one_day_passed_given_BackstagePass_sellIn_1_quality_1() {
         BackstagePass backstagePass = new BackstagePass(1,1);
         Rose rose = new Rose();
-        BackstagePass updatedAgedBrie = (BackstagePass) rose.oneDayPassed(backstagePass);
+        BackstagePass updatedBackstagePass = (BackstagePass) rose.oneDayPassed(backstagePass);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(4);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(0);
+        assertThat(updatedBackstagePass.getQuality()).isEqualTo(4);
+        assertThat(updatedBackstagePass.getSellIn()).isEqualTo(0);
     }
 
     @Test
     void should_sellIn_minus_1_quality_0_when_one_day_passed_given_BackstagePass_sellIn_0_quality_1() {
         BackstagePass backstagePass = new BackstagePass(0,1);
         Rose rose = new Rose();
-        BackstagePass updatedAgedBrie = (BackstagePass) rose.oneDayPassed(backstagePass);
+        BackstagePass updatedBackstagePass = (BackstagePass) rose.oneDayPassed(backstagePass);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(0);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(-1);
+        assertThat(updatedBackstagePass.getQuality()).isEqualTo(0);
+        assertThat(updatedBackstagePass.getSellIn()).isEqualTo(-1);
     }
 
     @Test
     void should_sellIn_1_quality_50_when_one_day_passed_given_BackstagePass_sellIn_2_quality_49() {
         BackstagePass backstagePass = new BackstagePass(2,49);
         Rose rose = new Rose();
-        BackstagePass updatedAgedBrie = (BackstagePass) rose.oneDayPassed(backstagePass);
+        BackstagePass updatedBackstagePass = (BackstagePass) rose.oneDayPassed(backstagePass);
 
-        assertThat(updatedAgedBrie.getQuality()).isEqualTo(50);
-        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(1);
+        assertThat(updatedBackstagePass.getQuality()).isEqualTo(50);
+        assertThat(updatedBackstagePass.getSellIn()).isEqualTo(1);
     }
 
     @Test
